94367dd5da896ea363a58df8e7100d1d8e4e6cc5
indy1515 <indy1_thailand@hotmail.com>
2015-11-13 22:13:53 +0700

    Change constant

--- RainyWords/src/com/indyzalab/rainywords/gameplay/Constants.java
+++ RainyWords/src/com/indyzalab/rainywords/gameplay/Constants.java
@@ -4,8 +4,8 @@ public class Constants {
 	public static final int GAMEUI_WIDTH = 800;
 	public static final int GAMEUI_HEIGHT = 500;
 	public static final int WORD_DELAY = 1000; // Delay between word in millis
-	public static final int GAME_TIME = 5*60*1000;
-//	public static final int GAME_TIME = 30000; 
+//	public static final int GAME_TIME = 5*60*1000;
+	public static final int GAME_TIME = 30000; 
 	public static final int FALL_SPEED = 10; // The greater the slower speed of the word
 	public static final int PIXEL_Y_AXIS_SPEED = 1;
 }
